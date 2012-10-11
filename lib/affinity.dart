b250b184c9546ed4bb54120917d84ba75e10c949
rmsmith <ross@honey>
2012-10-10 21:07:09 -0700

    moving non-library files into /src subdir

--- lib/affinity.dart
+++ lib/affinity.dart
@@ -3,6 +3,6 @@ library affinity;
 
 import 'dart:math';
 
-part 'affine_transform.dart';
-part 'rectangle.dart';
-part 'vector.dart';
+part 'src/affine_transform.dart';
+part 'src/rectangle.dart';
+part 'src/vector.dart';
