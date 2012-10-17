bbf5b37d3e8d60b6005c9f08c1c7e038a7a108e8
rmsmith <ross@honey>
2012-10-17 08:44:28 -0700

    top-level library does not need to 'import' in order to 'export'
    only needs an 'import' if it uses the code , as I learned by randomly
    reading issue 5771 in the dartlang tracker this AM

--- lib/affinity.dart
+++ lib/affinity.dart
@@ -3,11 +3,6 @@ library affinity;
 
 import 'dart:math';
 
-import 'affine_transform.dart';
 export 'affine_transform.dart';
-
-import 'rectangle.dart';
 export 'rectangle.dart';
-
-import 'vector.dart';
 export 'vector.dart';
