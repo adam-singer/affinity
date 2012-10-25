daf198e02251313665fd24ea60bd2dc9e03f895c
rmsmith <ross@honey>
2012-10-24 21:39:39 -0700

    adding initial AffineTransform benchmarks

---
+++ test/affine_transform_bench.dart
@@ -0,0 +1,37 @@
+
+Benchmark affineTransformConcatenate() {
+  var tx = new AffineTransform(1, 2, 3, 4, 5, 6);  
+  var other = new AffineTransform(2, 1, 6, 5, 4, 3);
+  return new Benchmark(() => tx.concatenate(other));
+}
+
+Benchmark affineTransformPreconcatenate() {
+  var tx = new AffineTransform(1, 2, 3, 4, 5, 6);  
+  var other = new AffineTransform(2, 1, 6, 5, 4, 3);
+  return new Benchmark(() => tx.preconcatenate(other));
+}
+
+Benchmark affineTransformRotate() {    
+  var tx = new AffineTransform(1, 2, 3, 4, 5, 6);
+  return new Benchmark(() => tx.rotate(PI/4));
+}
+
+Benchmark affineTransformRotateAnchor() {    
+  var tx = new AffineTransform(1, 2, 3, 4, 5, 6);
+  return new Benchmark(() => tx.rotateAnchor(PI/4, 2, 3));
+}
+
+Benchmark affineTransformScale() {    
+  var tx = new AffineTransform(1, 2, 3, 4, 5, 6);
+  return new Benchmark(() => tx.scale(2, 3));
+}
+
+Benchmark affineTransformShear() {    
+  var tx = new AffineTransform(1, 2, 3, 4, 5, 6);
+  return new Benchmark(() => tx.shear(2, 3));
+}
+
+Benchmark affineTransformTranslate() {   
+  var tx = new AffineTransform(1, 2, 3, 4, 5, 6);
+  return new Benchmark(() => tx.translate(2, 3));
+}
---
+++ test/affinity_bench.dart
@@ -0,0 +1,14 @@
+
+library affinity_bench;
+
+import 'dart:math';
+import 'package:affinity/affinity.dart';
+import 'package:bench/bench.dart';
+import 'package:logging/logging.dart';
+
+part 'affine_transform_bench.dart';
+
+void main() {
+  Logger.root.on.record.add((record) => print('${record.message}'));
+  new Benchmarker().run(iterations:1000);
+}
